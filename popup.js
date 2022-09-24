274475a1cfa5d3650d5a695b30a2d0e658130ac7
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-09-24 16:05:50 +0200

    feat: UI overhaul

---
+++ popup.js
@@ -0,0 +1,20 @@
+
+document.getElementById('impbtn').addEventListener('click', () => {
+    browser.windows.create({
+        url: ["import-dialog.html"],
+        type: "popup",
+        width: 300,
+        height: 250
+    });
+});
+
+
+document.getElementById('expbtn').addEventListener('click', () => {
+    browser.windows.create({
+        url: ["export-dialog.html"],
+        type: "popup",
+        width: 300,
+        height: 250
+    });
+});
+
