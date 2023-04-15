c56217e55fc0c19b49172f4a4ee8a5ed15272650
igorlogius <67047467+igorlogius@users.noreply.github.com>
2023-04-15 13:52:41 +0200

    fix: add default_area attribute

--- popup.js
+++ popup.js
@@ -1,21 +1,19 @@
 /* global browser */
 
-document.getElementById('impbtn').addEventListener('click', () => {
-    browser.windows.create({
-        url: ["import-dialog.html"],
-        type: "popup",
-        width: 300,
-        height: 250
-    });
+document.getElementById("impbtn").addEventListener("click", () => {
+  browser.windows.create({
+    url: ["import-dialog.html"],
+    type: "popup",
+    width: 300,
+    height: 250,
+  });
 });
 
-
-document.getElementById('expbtn').addEventListener('click', () => {
-    browser.windows.create({
-        url: ["export-dialog.html"],
-        type: "popup",
-        width: 300,
-        height: 250
-    });
+document.getElementById("expbtn").addEventListener("click", () => {
+  browser.windows.create({
+    url: ["export-dialog.html"],
+    type: "popup",
+    width: 300,
+    height: 250,
+  });
 });
-
